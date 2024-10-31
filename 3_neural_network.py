6b77acbf4db4536424c8409f9ea1b833df31dbd1
liuxiang <liuxiang09102021@163.com>
2024-10-31 11:40:01 +0800

    卷积神经网络

---
+++ 3_neural_network.py
@@ -0,0 +1,56 @@
+"""
+一个典型的神经网络训练过程包括以下几点：
+
+1.定义一个包含可训练参数的神经网络
+
+2.迭代整个输入
+
+3.通过神经网络处理输入
+
+4.计算损失(loss)
+
+5.反向传播梯度到神经网络的参数
+
+6.更新网络的参数，典型的用一个简单的更新方法：weight = weight - learning_rate *gradient
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        # 定义了两个卷积层conv1和conv2，接着是三个全连接层fc1、fc2和fc3
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    # 向前传播函数
+    def forward(self, x):
+        # Max pooling over a (2, 2) window
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        # If the size is a square you can only specify a single number
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    # 计算展平后的特征数量
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+
+net = Net()
+print(net)
